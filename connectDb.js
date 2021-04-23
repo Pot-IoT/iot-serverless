3959ec98c6dc124fbc7647b7f98e37e10fed1697
hailong <hailong_yang@comcast.com>
2021-04-23 09:20:56 -0500

    create db connection helper

---
+++ connectDb.js
@@ -0,0 +1,32 @@
+const AWS = require("aws-sdk");
+const mysql = require("mysql");
+
+const secretsManager = new AWS.SecretsManager({ region: "ap-east-1" });
+
+const connectDb = () => {
+  const promise = new Promise((resolve, reject) => {
+    secretsManager.getSecretValue(
+      { SecretId: "prod/IOT/MySQL" },
+      (err, data) => {
+        if (err) reject(err);
+
+        const { username, password, host, dbname } = JSON.parse(
+          data.SecretString
+        );
+
+        const connection = mysql.createConnection({
+          host,
+          user: username,
+          password,
+          database: dbname,
+        });
+
+        resolve(connection);
+      }
+    );
+  });
+
+  return promise;
+};
+
+exports.connectDb = connectDb;
